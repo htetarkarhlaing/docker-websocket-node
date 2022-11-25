9c43a7c0e85d4fd9be9b024f1150a33dcedcc304
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-11-25 12:04:04 +0630

    v1: initial setup

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:alpine
+WORKDIR /usr/src/app
+COPY package*.json .
+RUN npm ci
+COPY . .
+RUN npm run build
+CMD ["npm", "start"]
---
+++ server.ts
@@ -0,0 +1,54 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { WebSocketServer } from "ws";
+import { v4 as uuidV4 } from "uuid";
+
+// instances
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
+const PORT = parseInt(process.env.PORT.toString(), 10) || 8000;
+
+const server = app.listen(PORT, () => {
+	console.log("Server is started.");
+});
+
+server.on("upgrade", (request, socket, head) => {
+	wsServer.handleUpgrade(request, socket, head, (socket) => {
+		wsServer.emit("connection", socket, request);
+	});
+});
+
+let CLIENTS = [];
+
+// web socket stuff
+const wsServer = new WebSocketServer({ noServer: true });
+wsServer.on("connection", (socket) => {
+	let id = uuidV4();
+	CLIENTS[id] = socket;
+	CLIENTS.push(socket);
+	socket.on("message", (msg) => {
+		let incomingMsg = JSON.parse(JSON.stringify(msg.toString()));
+		sendAll(incomingMsg);
+	});
+
+	socket.send("Hello Flutter!");
+});
+
+// send message to all
+const sendAll = (message: any) => {
+	for (let i = 0; i < CLIENTS.length; i++) {
+		CLIENTS[i].send(message);
+	}
+};
